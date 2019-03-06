52fed7ab694d6bfca4e5cbf9ebc8dffa7b5400bb
Mike <sackmike.093@gmail.com>
2019-03-06 10:07:36 +0100

    MULTI IDIOMA

--- app/src/main/java/com/example/happyvoice/MainActivity.java
+++ app/src/main/java/com/example/happyvoice/MainActivity.java
@@ -6,6 +6,7 @@ import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,12 +15,16 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     TextView grabar;
+    Button idi;
+    int idioma;
     private static final int RECOGNIZE_SPEECH_ACTIVITY = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         grabar = findViewById(R.id.txtGrabarVoz);
+        idi = findViewById(R.id.b_idioma);
+        idioma=1;
     }
 
     @Override
@@ -31,7 +36,19 @@ public class MainActivity extends AppCompatActivity {
                     ArrayList<String> speech = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     String strSpeech2Text = speech.get(0);
-                    grabar.setText("Voz reconocida:\n"+strSpeech2Text);
+                    switch (idioma){
+                        case 1:
+                            grabar.setText("Voz reconocida:\n"+strSpeech2Text);
+                            break;
+                        case 2:
+                            grabar.setText("Recognized voice:\n"+strSpeech2Text);
+                            break;
+                        case 3:
+                            grabar.setText("Veu reconeguda:\n"+strSpeech2Text);
+                            break;
+
+
+                    }
                 }
                 break;
             default:
@@ -41,8 +58,22 @@ public class MainActivity extends AppCompatActivity {
     public void onClickImgBtnHablar(View v) {
         Intent intentActionRecognizeSpeech = new Intent(
                 RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intentActionRecognizeSpeech.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE_MODEL, "es-ES");
+        switch (idioma){
+            case 1:
+                intentActionRecognizeSpeech.putExtra(
+                        RecognizerIntent.EXTRA_LANGUAGE, "es-ES");
+                break;
+            case 2:
+                intentActionRecognizeSpeech.putExtra(
+                        RecognizerIntent.EXTRA_LANGUAGE, "en-US");
+                break;
+            case 3:
+                intentActionRecognizeSpeech.putExtra(
+                        RecognizerIntent.EXTRA_LANGUAGE, "ca-ES");
+                break;
+
+        }
+
         try {
             startActivityForResult(intentActionRecognizeSpeech,
                     RECOGNIZE_SPEECH_ACTIVITY);
@@ -53,4 +84,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void idioma(View view) {
+        idioma++;
+        if(idioma==4){idioma=1;}
+        switch (idioma){
+            case 1:
+                idi.setText("ESP");
+                break;
+            case 2:
+                idi.setText("ENG USA");
+                break;
+            case 3:
+                idi.setText("CAT");
+                break;
+        }
+    }
 }
